b8b9658c109bf1a637c9f652e25388a83506c32d
Przemysław Orzechowski <przemeklokfoam04@gmail.com>
2026-05-12 21:32:19 +0200

    Update monitor_loop.py

--- monitor_loop.py
+++ monitor_loop.py
@@ -1,10 +1,8 @@
 import requests
-import hashlib
 import os
 import time
 
-URL = "https://test1874.reservio.com/events"
-CHECK_EVERY_SECONDS = 60
+URL = "https://ttsd.reservio.com/events"
 
 TELEGRAM_TOKEN = os.environ["TELEGRAM_TOKEN"]
 CHAT_ID = os.environ["CHAT_ID"]
@@ -19,50 +17,21 @@ def notify(text):
         timeout=20
     )
 
-def get_hash():
-    response = requests.get(
-        URL,
-        timeout=20,
-        headers={
-            "User-Agent": "Mozilla/5.0"
-        }
-    )
+response = requests.get(
+    URL,
+    headers={
+        "User-Agent": "Mozilla/5.0"
+    },
+    timeout=20
+)
 
-    html = response.text
+text = response.text[:3500]
 
-    important = []
-
-    for line in html.splitlines():
-        line_lower = line.lower()
-
-        if "dostępn" in line_lower:
-            important.append(line.strip())
-
-    content = "\n".join(important)
-
-    return hashlib.sha256(
-        content.encode("utf-8")
-    ).hexdigest()
-
-old_hash = None
-
-notify("✅ Monitor Reservio uruchomiony.")
+notify(
+    "TTSD TEST:\n\n"
+    + text
+)
 
 while True:
-    try:
-        current_hash = get_hash()
-
-        if old_hash and old_hash != current_hash:
-            notify(
-                "🚨 Wykryto wolne miejsce na Reservio!\n"
-                + URL
-            )
-
-        old_hash = current_hash
-
-        print("Sprawdzono stronę.")
-
-    except Exception as e:
-        print("Błąd:", e)
-
-    time.sleep(CHECK_EVERY_SECONDS)
+    print("Test działa.")
+    time.sleep(60)
